3cbb7125080f0d2c61cc6022cb971599ab3c9d12
Alejandro Rodriguez <arodriguez@tribalscale.com>
2019-10-13 15:34:52 +0400

    feat: 🎸 Monitoring sample (micrometer + prometheus)

---
+++ another-app/src/main/java/app/main/controller/MonitoredController.java
@@ -0,0 +1,28 @@
+package app.main.controller;
+
+import io.micrometer.core.annotation.Timed;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+@RestController
+public class MonitoredController {
+
+    @Timed
+    @GetMapping("/myFood")
+    public String getFood() throws Exception {
+        List<String> food = Arrays.asList("potato", "lettuce", "tomato", "brick");
+
+        Random rand = new Random();
+        String dish = food.get(rand.nextInt(food.size()));
+
+        if (dish.equalsIgnoreCase("brick")) {
+            throw new Exception("That's no food");
+        } else {
+            return dish;
+        }
+    }
+}
